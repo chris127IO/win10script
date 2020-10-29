d9b67955580e6853f45212b29e9d47482d7ebaf1
chris127IO <73670798+chris127IO@users.noreply.github.com>
2020-10-29 22:33:28 +0100

    Update win10debloat.ps1

--- win10debloat.ps1
+++ win10debloat.ps1
@@ -8,7 +8,7 @@
 #
 #	Addition: One command to rule them all, One command to find it, and One command to Run it! 
 #
-#     > powershell -nop -c "iex(New-Object Net.WebClient).DownloadString('https://git.io/JJ8R4')"
+#     > powershell -nop -c "iex(New-Object Net.WebClient).DownloadString('https://git.io/JTyvu')"
 #
 #	Chris Titus Tech Additions:
 #
